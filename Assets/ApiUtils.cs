242d9d7d6e73f22754c573841e283e060df10a60
Zayshaa <Cara.di42@gmail.com>
2022-10-28 21:51:54 -0700

    txt2img basics working, progress bars and things

---
+++ Assets/ApiUtils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine.Networking;
+
+public static class ApiUtils
+{
+    public static IEnumerator ApiRequest(int functionId, Dictionary<string, object> parameters = null, Action<string> callback = null)
+    {
+        if (parameters is null) parameters = new();
+        parameters.Add("fn_index", functionId);
+
+        var reqBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(parameters, Formatting.Indented));
+
+        var webRequest = new UnityWebRequest("http://localhost:7860/api/predict/", UnityWebRequest.kHttpVerbPOST);
+
+        DownloadHandler dH = new DownloadHandlerBuffer();
+        webRequest.downloadHandler = dH;
+        webRequest.uploadHandler   = new UploadHandlerRaw(reqBytes);
+        webRequest.useHttpContinue = false;
+        webRequest.SetRequestHeader("Content-Type", "application/json");
+
+        yield return webRequest.SendWebRequest();
+
+        if (webRequest.result == UnityWebRequest.Result.Success)
+        {
+            // Debug.Log(dH.text);
+            var reqJson = JObject.Parse(dH.text);
+        }
+
+        callback?.Invoke(dH.text);
+    }
+}
---
+++ Assets/Editor/MaskEditor.cs
@@ -0,0 +1,7 @@
+﻿namespace Editor.Resources
+{
+    public class MaskEditor : UnityEditor.EditorWindow
+    {
+               
+    }
+}
