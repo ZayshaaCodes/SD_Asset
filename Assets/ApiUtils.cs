18db4020498dafdbd8be2ff643d4d59abb75c794
Zayshaa <Cara.di42@gmail.com>
2022-10-28 21:52:06 -0700

    missed some

--- Assets/ApiUtils.cs
+++ Assets/ApiUtils.cs
@@ -4,14 +4,59 @@ using System.Collections.Generic;
 using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Unity.EditorCoroutines.Editor;
+using UnityEngine;
 using UnityEngine.Networking;
 
 public static class ApiUtils
 {
-    public static IEnumerator ApiRequest(int functionId, Dictionary<string, object> parameters = null, Action<string> callback = null)
+    public enum ApiFunction
+    {
+        GetModels = 0, GetSamplers = 289, GetCurModel = 317, SetModel = 302
+    }
+
+    public static void GetModels(List<string> models)
+    {
+        models.Clear();
+        EditorCoroutineUtility.StartCoroutineOwnerless(ApiRequest(ApiFunction.GetModels, null, (s, o) =>
+        {
+            if (o["data"]?[0]?["choices"] is { } choices)
+            {
+                foreach (var choice in choices)
+                {
+                    models.Add(choice.ToString());
+                }
+            }
+        }));
+    }
+
+    public static void SetModel(string model)
+    {
+        EditorCoroutineUtility.StartCoroutineOwnerless(ApiRequest(ApiFunction.SetModel, new() { { "data", new List<string>() {model} } }));
+    }
+
+    public static void GetSamplers(List<string> samplers)
+    {
+        samplers.Clear();
+        EditorCoroutineUtility.StartCoroutineOwnerless(ApiRequest(ApiFunction.GetSamplers, null, (s, o) =>
+        {
+            if (o["data"]?[0] is { } choices)
+            {
+                foreach (var choice in choices)
+                {
+                    samplers.Add(choice.ToString());
+                }
+            }
+        }));
+    }
+
+    public static IEnumerator ApiRequest(ApiFunction function, Dictionary<string, object> parameters = null, Action<string, JObject> callback = null) =>
+        ApiRequest((int)function, parameters, callback);
+
+    public static IEnumerator ApiRequest(int function, Dictionary<string, object> parameters = null, Action<string, JObject> callback = null)
     {
         if (parameters is null) parameters = new();
-        parameters.Add("fn_index", functionId);
+        parameters.Add("fn_index", function);
 
         var reqBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(parameters, Formatting.Indented));
 
@@ -25,12 +70,20 @@ public static class ApiUtils
 
         yield return webRequest.SendWebRequest();
 
+        //Debug.Log(webRequest.result);
         if (webRequest.result == UnityWebRequest.Result.Success)
         {
             // Debug.Log(dH.text);
             var reqJson = JObject.Parse(dH.text);
+            callback?.Invoke(dH.text, reqJson);
         }
+    }
 
-        callback?.Invoke(dH.text);
+    public static IEnumerator GetCurModel(Action<string> callback)
+    {
+        yield return ApiRequest(ApiFunction.GetCurModel, null, (s, jo) =>
+        {
+            callback?.Invoke(jo["data"]?[0]?.ToString());
+        });
     }
 }
--- Assets/Editor/SD_Editor.cs
+++ Assets/Editor/SD_Editor.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using Codice.Client.BaseCommands;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Unity.EditorCoroutines.Editor;
@@ -14,24 +16,74 @@ using UnityEngine.UIElements;
 
 namespace Editor
 {
+    public enum Samplers
+    {
+        Euler_A, Euler,
+    }
+
     public class SD_Editor : EditorWindow
     {
-        [SerializeField]                          private string message;
-        [SerializeField]                          private string prompt     = "pixar character";
-        [SerializeField]                          private string negprompt  = "";
-        [SerializeField, Range(1, 50)]            private int    steps      = 25;
-        [SerializeField, Range(1, 10)]            private int    batchCount = 2;
-        [SerializeField, Range(1, 30)]            private float  cfgScale   = 10;
-        [SerializeField, RangeEx(128, 1024, 128)] private int  width      = 512;
-        [SerializeField, RangeEx(128, 1024, 128)] private int  height     = 512;
+        [SerializeField]               private string prompt     = "pixar character";
+        [SerializeField]               private string negprompt  = "";
+        [SerializeField, Range(1, 50)] private int    steps      = 25;
+        [SerializeField, Range(1, 10)] private int    batchCount = 1;
+        [SerializeField, Range(1, 10)] private int    batchSize  = 2;
+        [SerializeField, Range(1, 30)] private float  cfgScale   = 10;
+        [SerializeField, Range(0, 1)]  private float  denoise    = .5f;
 
-        private VisualElement outContainer;
+        [SerializeField] private int seed = -1;
+
+        [SerializeField]                private string message;
+        [SerializeField, Range(0f, 1f)] private float  progress;
+
+        private MaskEditor maskEditWindow => GetWindow<MaskEditor>();
+
+        [SerializeField, RangeEx(128, 1024, 128)]
+        private int width = 512;
+
+        [SerializeField, RangeEx(128, 1024, 128)]
+        private int height = 512;
+
+        private VisualElement   outContainer;
+        private List<Texture2D> outputImages;
+        private Texture2D       mask;
+
+        private                  bool          isRunning;
+        private                  VisualElement previewElement;
+        private                  Texture2D     previewTexture;
+        [SerializeField] private List<string>  models = new();
+
+        [SerializeField] private List<string> samplers = new()
+        {
+            "Euler a",
+            "Euler",
+            "LMS",
+            "Heun",
+            "DPM adaptive",
+            "DDIM",
+            "PLMS"
+        };
+
+        [SerializeField] private string selectedSampler = "Euler a";
 
         [MenuItem("SD/Window")]
         public static void LoadWindow()
         {
             SD_Editor window = CreateWindow<SD_Editor>();
             window.Show();
+        }
+
+        private void OnGUI()
+        {
+            // message = Event.current.mousePosition.ToString();
+        }
+
+        private void Update()
+        {
+            if (isRunning)
+            {
+                Repaint();
+            }
         }
 
         private void CreateGUI()
@@ -41,29 +93,75 @@ namespace Editor
             vta.CloneTree(root);
             root.Bind(new SerializedObject(this));
 
-            var generateButton = root.Q<Button>("Generate_btn");
+            previewTexture = new Texture2D(512, 512);
+            previewElement = new VisualElement() { style = { backgroundImage = previewTexture, width = 512, height = 512 } };
 
-            var sv = root.Q<ScrollView>();
-            outContainer = sv.Q<VisualElement>("unity-content-container");
+            ApiUtils.GetModels(models);
 
-            generateButton.clicked += () =>
+            if (root.Q<DropdownField>("model_dropdown") is { } dropdownField)
             {
-                Debug.Log("generate: " + prompt);
-                EditorCoroutineUtility.StartCoroutine(test(), this);
-            };
+                dropdownField.choices = models;
+                EditorCoroutineUtility.StartCoroutine(ApiUtils.GetCurModel(s => dropdownField.value = s), this);
+
+                dropdownField.RegisterValueChangedCallback(evt =>
+                {
+                    ApiUtils.SetModel(evt.newValue);
+                });
+            }
+
+            if (root.Q<DropdownField>("samplers_dropdown") is { } samplerField)
+            {
+                samplerField.choices = samplers;
+                samplerField.value   = samplers[0];
+            }
+
+            if (root.Q<ScrollView>() is { } sv)
+            {
+                if (sv.Q<VisualElement>("unity-content-container") is { } outCon)
+                {
+                    outContainer = outCon;
+                    outContainer.Add(previewElement);
+                }
+            }
+
+            if (root.Q<Button>("maskeditor_btn") is { } maskButton)
+            {
+                maskButton.clicked += () =>
+                {
+                    CreateWindow<MaskEditor>();
+                };
+            }
+
+
+            if (root.Q<Button>("generate_btn") is { } genButton)
+            {
+                genButton.clicked += () =>
+                {
+                    Debug.Log("generate: " + prompt);
+                    previewElement.style.display = DisplayStyle.Flex;
+                    EditorCoroutineUtility.StartCoroutine(Generate(), this);
+                };
+            }
+
+            // var cancelButton = root.Q<Button>("Interupt_btn");
+            // cancelButton.clicked += () =>
+            // {
+            //     EditorCoroutineUtility.StartCoroutine(ApiUtils.ApiRequest(2), this);
+            //     isRunning = false;
+            // };
         }
 
-        private IEnumerator test()
+        private IEnumerator Generate()
         {
             Dictionary<string, object> postData = new()
             {
                 { "enable_hr", false },
-                { "denoising_strength", 0 },
+                { "denoising_strength", denoise },
                 { "firstphase_width", 0 },
                 { "firstphase_height", 0 },
                 { "prompt", prompt },
-                { "styles", new List<string>() { "string" } },
-                { "seed", -1 },
+                // { "styles", new List<string>() },
+                { "seed", seed },
                 { "subseed", -1 },
                 { "subseed_strength", 0 },
                 { "seed_resize_from_h", -1 },
@@ -82,7 +180,7 @@ namespace Editor
                 { "s_tmax", 0 },
                 { "s_tmin", 0 },
                 { "s_noise", 1 },
-                { "sampler_index", "Euler A" }
+                { "sampler_index", selectedSampler }
             };
 
             var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(postData, Formatting.Indented));
@@ -97,20 +195,21 @@ namespace Editor
             request.useHttpContinue = false;
             request.SetRequestHeader("Content-Type", "application/json");
 
+            isRunning = true;
+            EditorCoroutineUtility.StartCoroutine(ProgressCheck(), this);
             yield return request.SendWebRequest();
 
-            for (int i = outContainer.childCount - 1; i >= 0; i--)
-            {
-                outContainer.RemoveAt(i);
-            }
+            ClearOutputContainer();
+            previewElement.style.display = new StyleEnum<DisplayStyle>(DisplayStyle.None);
+            isRunning                    = false;
+            progress                     = 1;
 
-            message = "result:" + request.result;
             if (request.result == UnityWebRequest.Result.Success)
             {
+                // Debug.Log(dH.text);
                 var reqJson     = JObject.Parse(dH.text);
                 var imagesToken = reqJson["images"];
                 var c           = imagesToken!.Count();
-
                 for (int i = 0; i < c; i++)
                 {
                     var t2d      = new Texture2D(width, height);
@@ -118,17 +217,57 @@ namespace Editor
 
                     var imgData = Convert.FromBase64String(imageStr);
                     t2d.LoadImage(imgData);
-                    Debug.Log(outContainer);
-                    outContainer.Add(new VisualElement()
+
+                    var newImg = new VisualElement()
                     {
                         style =
                         {
                             backgroundImage = t2d,
-                            width           = width/2,
-                            height          = height/2
+                            width           = 256,
+                            height          = 256
                         }
+                    };
+                    newImg.RegisterCallback<MouseDownEvent>(evt =>
+                    {
+                        maskEditWindow.SetPreview(t2d);
+                        maskEditWindow.SetPreview(t2d);
                     });
+                    outContainer.Add(newImg);
                 }
+            }
+        }
+
+        private void ClearOutputContainer()
+        {
+            for (int i = outContainer.childCount - 1; i >= 1; i--)
+            {
+                outContainer.RemoveAt(i);
+            }
+        }
+
+        private IEnumerator ProgressCheck()
+        {
+            while (isRunning)
+            {
+                yield return new EditorWaitForSeconds(.125f);
+
+                yield return ApiUtils.ApiRequest(4, null, (s, JObject) =>
+                {
+                    var match = Regex.Match(s, @";width:-?([\d\.]+)%;");
+                    if (float.TryParse(match.Groups[1].ToString(), out float val))
+                    {
+                        progress = val / 100;
+                    }
+                });
+
+                yield return ApiUtils.ApiRequest(3, null, (s, jo) =>
+                {
+                    var imgString = jo["data"]?[2]?.ToString();
+                    if (imgString.Length > 500 && imgString.Substring(22, imgString.Length - 22) is { } imgData)
+                    {
+                        previewTexture.LoadImage(Convert.FromBase64String(imgData));
+                    }
+                });
             }
         }
     }
